e6e9c8a41a4f1f68c66e1aa71932a316a0d3f1be
SLain <sl163@ya.ru>
2020-11-26 19:07:47 +0300

    Добавляет чистый проект;

---
+++ source/js/main.js
@@ -0,0 +1,11 @@
+import {polyfills} from "./utils/polyfills";
+
+// Utils
+// ---------------------------------
+
+polyfills();
+
+// Modules
+// ----------------------------------
+
+console.log('Works!');
